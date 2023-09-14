f9a0a8756a426fdc5c0ff9209392d5c690dd4211
Hendrick <70229868+EZSNoVa@users.noreply.github.com>
2023-09-14 20:16:04 +0000

    Setting up homepage, started idea for auth page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col justify-center items-center h-screen">
+	<main>
+		<h1 class="m-0 mb-1 text-4xl font-bold"><span class="blink p-1">NoVa</span> Auth</h1>
+		<p class="text-center">A simple authentication system.</p>
+
+        <div class="border rounded-sm flex flex-row justify-evenly items-center w-full p-2">
+            <div title="Create an account or log in"  class="w-1/3 justify-center items-center">
+                <p class="m-0 text-center hover:bg-white hover:text-slate-950">Join</p>
+            </div>
+
+            <span class="font-bold select-none"> | </span>
+
+            <div title="Start using NoVa Auth Service" class="w-1/3 justify-center items-center">
+                <p class="m-0 text-center hover:bg-white hover:text-slate-950">Use</p>
+            </div>
+        </div>
+    </main>
+</div>
+
+<footer class="absolute w-full text-center">
+	<p class="text-sm text-gray-500">
+		Made with <span class="text-red-500">&hearts;</span> by
+		<a href="https://github.com/ezsnova/">NoVa</a>
+	</p>
+</footer>
+
+<style>
+	.blink {
+		animation: blinker 2.5s cubic-bezier(0.755, 0.05, 0.855, 0.06) infinite;
+	}
+	@keyframes blinker {
+		50% {
+			opacity: 0;
+			border-bottom: 2px solid #7d1fc2;
+		}
+	}
+</style>
---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+	import Fa from 'svelte-fa';
+	import { faGithub } from '@fortawesome/free-brands-svg-icons';
+	import { faCertificate } from '@fortawesome/free-solid-svg-icons';
+</script>
+
+<div class="w-full h-screen flex justify-centera items-center">
+	<main class="py-4 mx-auto w-10/12 max-w-[66%] flex flex-col items-center justify-center">
+		<div class="glass-card p-4 w-full">
+			<div class="flex justify-center">
+				<div class="rounded-full overflow-hidden border-4 border-white">
+					<img
+						alt="Nova Auth logo"
+						class="w-32 h-32"
+						src="https://avatars.githubusercontent.com/u/25180623?v=4"
+					/>
+				</div>
+			</div>
+
+			<div class="flex flex-col items-center justify-center mt-4">
+				<div class="flex flex-row items-center justify-evenly">
+					<h1 class="m-0 text-2xl font-bold text-center">NoVa Auth</h1>
+
+					<i title="Verified">
+						<Fa class="m-2 text-purple-500" icon={faCertificate} />
+					</i>
+				</div>
+				<p class="text-center">A simple authentication system.</p>
+			</div>
+		</div>
+
+		<a href="https://github.com/ezsnova/nova-auth">
+			<Fa icon={faGithub} class="text-white text-4xl mt-4" />
+		</a>
+	</main>
+</div>
+
+<style>
+	.glass-card {
+		background: rgba(125, 31, 194, 0.15);
+		box-shadow: 0 rgba(129, 52, 185, 0.15);
+		backdrop-filter: blur(5px);
+		-webkit-backdrop-filter: blur(5px);
+		border-radius: 10px;
+	}
+</style>
